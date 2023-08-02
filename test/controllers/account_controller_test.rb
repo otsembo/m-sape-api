20febb1b4eb70d718a4ba7c3972b814a45d99488
otsembo <okumu.otsembo@gmail.com>
2023-08-02 10:21:29 +0300

    Add integration tests

--- test/controllers/account_controller_test.rb
+++ test/controllers/account_controller_test.rb
@@ -1,7 +1,45 @@
 require "test_helper"
 
 class AccountControllerTest < ActionDispatch::IntegrationTest
-  # test "the truth" do
-  #   assert true
-  # end
+
+  party_a = "sample@mail.com"
+  party_b = "mail@mail.com"
+  account_params = { account: { "amount" => 200 } }
+  transfer_params = { account: { "amount" => 200, "user_mail" => party_b } }
+
+  test "top up account" do
+    token = create_account
+    post "/account/topup", params: account_params , headers: { "Authorization" => "Bearer #{token}"}
+    assert_response :success
+    post "/account/topup", params: account_params
+    assert_response :forbidden
+  end
+
+  test "withdraw account" do
+    token = create_account
+    post "/account/topup", params: account_params , headers: { "Authorization" => "Bearer #{token}"}
+    post "/account/withdraw", params: account_params, headers: { "Authorization" => "Bearer #{token}"}
+    assert_response :success
+    post "/account/withdraw", params: account_params
+    assert_response :forbidden
+  end
+
+  test "transfer money" do
+    token = create_account
+    post "/account/topup", params: account_params, headers: { "Authorization" => "Bearer #{token}"}
+    assert_no_difference("Transaction.count") do
+      post "/account/transfer", params: transfer_params, headers: { "Authorization" => "Bearer #{token}"}
+    end
+  end
+
+  private
+
+  def create_account
+    auth_params = { email: "mail@mail.com", password: "helloWorld", phone: "0701010102" }
+    auth_params2 = { email: "sample@mail.com", password: "<PASSWORD>", phone: "0711111011"}
+    post "/auth/create_account", params: auth_params
+    post "/auth/create_account", params: auth_params2
+    JSON.parse(@response.body)["data"]["auth"]
+  end
+
 end
--- test/controllers/auth_controller_test.rb
+++ test/controllers/auth_controller_test.rb
@@ -1,7 +1,23 @@
 require "test_helper"
 
 class AuthControllerTest < ActionDispatch::IntegrationTest
-  # test "the truth" do
-  #   assert true
-  # end
+
+  auth_params = { email: "mail@mail.com", password: "helloWorld", phone: "0701010102" }
+
+  test "create account" do
+    assert_difference("User.count") do
+      post "/auth/create_account", params: auth_params
+      assert_response :created
+    end
+    assert_no_difference "User.count" do
+      post "/auth/create_account", params: { email: "mail@mail.com", password: "", phone: "0701010102" }
+    end
+  end
+
+  test "login" do
+    post "/auth/create_account", params: auth_params
+    post "/auth/login", params: auth_params
+    assert_response :ok
+  end
+
 end
